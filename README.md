14b68ef4790848bc4826239bb92681113d842cec
Kishwar Jabeen <kishwerjabeen90@gmail.com>
2023-10-31 16:27:06 +0500

    light dark button

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-# React + Vite
+# Light Dark Button
+# React + Vite + TailwindCss 
+
+
+# button icons
+https://heroicons.com/
+
+
+
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
--- src/components/LightAndDarkModeButton.jsx
+++ src/components/LightAndDarkModeButton.jsx
@@ -24,6 +24,7 @@ const LightAndDarkModeButton = () => {
 
             <div className='flex justify-center items-center h-screen'>
 
+                {/* light button */}
                 <button
                     onClick={toggleMode}
                     className='bg-gray-300 rounded-full p-2'>
@@ -36,6 +37,27 @@ const LightAndDarkModeButton = () => {
                     </svg>
 
                 </button>
+
+
+                {/* dark button */}
+
+
+                <button
+                    onClick={toggleMode}
+                    className='bg-gray-300 rounded-full p-2'>
+                    <svg xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-10 h-10">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" />
+                    </svg>
+
+                </button>
+
+
+
             </div>
         </div>
     )
